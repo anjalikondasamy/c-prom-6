f7bb1bbfbe7d0c0e34a207ef5f485626408ea1ff
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-12 03:45:09 -0700

    swapping.c

---
+++ swapping.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+if(a!=b)
+{
+printf("Enter the a:");
+scanf("%d",&a);
+printf("Enter the b:");
+scanf("%d",&b);
+}
+else
+{
+printf("swapping of two number");
+}
+}
